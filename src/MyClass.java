08b62dc8be157766fca6693a92f66da9b9a3dde6
unknown <aleksandarristov28@gmail.com>
2023-12-22 20:34:57 +0100

    first java program

---
+++ src/MyClass.java
@@ -0,0 +1,17 @@
+
+public class MyClass {
+	public static void main(String[]args) {
+		
+		Person j;
+		j= new Person("John");
+		j.setAge(20);
+		celebrateBirthday(j);
+		System.out.println(j.getAge());
+	}
+		static void celebrateBirthday(Person p) {
+			p.setAge(p.getAge() + 1);
+		}
+	
+	}
+	
+
---
+++ src/Person.java
@@ -0,0 +1,17 @@
+
+public class Person {
+	private String name;
+	private int age;
+	
+	public Person(String name) {
+		
+		this.name=name;
+		}
+	public void setAge(int age) {
+		this.age=age;
+	}
+	public int getAge() {
+		return age;
+	}
+
+}
